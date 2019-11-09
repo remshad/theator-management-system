609ee6c33b9ce42eee2654a27f7cb4ec10f68e24
IAmNithil <57283052+IAmNithil@users.noreply.github.com>
2019-11-09 11:27:20 +0530

    Revert "Update add_category.php"
    This reverts commit 0146c99da9ea048ddce756b79bc5fb368041ffeb.

--- admin/add_category.php
+++ admin/add_category.php
@@ -1,21 +1,17 @@
 <?php
 require('login1.php'); 
 
-$head=
-    "<script>
-    
-    
-    
-    
+$head="
+    <script>
         function add(myid)
         {
-        var new_cat=prompt('Enter new category name');
+        var nvalue=prompt('Enter new category name');
         
-        if(new_cat!=null)
-            {
-                myid.name=myid.name+'&new_cat='+new_cat;
-                window.location=myid.name;
-            }
+        if(nvalue!=null)
+        {
+        myid.name=myid.name+'&nvalue='+nvalue;
+        window.location=myid.name;
+        }
         }
         
         function edit(myid)
@@ -24,12 +20,12 @@ $head=
         var nvalue=prompt('Enter new category name');
         //myid.href=myid.href+'&nvalue='+nvalue;
 
-            if(nvalue!=null)
-            {
-            myid.name=myid.name+'&nvalue='+nvalue;
-            // alert(myid.name);
-            window.location=myid.name;
-            }        
+        if(nvalue!=null)
+        {
+        myid.name=myid.name+'&nvalue='+nvalue;
+       // alert(myid.name);
+        window.location=myid.name;
+        }        
         
         }
         
@@ -65,7 +61,7 @@ include_once('head.php');
                     $_GET['nvalue']=mysqli_real_escape_string($link,$_GET['nvalue']);
                     $_GET['cat_id']=intval($_GET['cat_id']);
                     urldecode($_GET['nvalue']);
-                    $sql="UPDATE category set name='{$_GET['nvalue']}' WHERE id='{$_GET['cat_id']}'";
+                    $sql="UPDATE  item_category set Name='{$_GET['nvalue']}' WHERE Cat_id='{$_GET['cat_id']}'";
                     $result=mysqli_query($link,$sql);
                     if(mysqli_error($link))
                     {
@@ -86,11 +82,16 @@ include_once('head.php');
                 
                 $cat_id=intval($_GET['cat_id']); 
 
-                $result1=mysqli_query($link,"SELECT * FROM category WHERE id='{$cat_id}'");
-                if(mysqli_num_rows($result1)==1)
+                $result1=mysqli_query($link,"SELECT * FROM item_generic WHERE Cat_id='{$cat_id}'");
+                $result2=mysqli_query($link,"SELECT * FROM item_special WHERE Cat_id='{$cat_id}'");
+                if(mysqli_num_rows($result1)>0 || mysqli_num_rows($result2)>1)
                 {
-                    //echo "deleting";
-                    $sql="DELETE FROM category WHERE id='{$cat_id}' ";
+                    $error.=" items avalable in item share table"; 
+                } 
+                else
+                {
+
+                    $sql="DELETE FROM item_category WHERE Cat_id='{$cat_id}' ";
                     mysqli_query($link,$sql);
                     if(mysqli_error($link))
                     {
@@ -105,13 +106,12 @@ include_once('head.php');
     {
         
        
-            if(isset($_POST['new_cat']) && strlen($_POST['new_cat'])>2)
+            if(isset($_POST['category']) && strlen($_POST['category'])>2)
             {
                 //echo '<script>alert("'.$_GET['nvalue'].'")</script>';
                 
-            $_POST['new_cat']=mysqli_real_escape_string($link,$_POST['new_cat']);
-                $sql="SELECT * FROM category WHERE name='{$_POST['new_cat']}'";
-                
+            $_POST['category']=mysqli_real_escape_string($link,$_POST['category']);
+                $sql="SELECT * FROM item_category WHERE Name='{$_POST['category']}'";
                 //echo $sql;
                 
                 $result = mysqli_query($link,$sql);
@@ -123,33 +123,34 @@ include_once('head.php');
                 if(mysqli_num_rows($result)>0)
                 {
                     echo '<script>alert("Duplicate Entry Not Allowed")</script>';
-                } else {
-                    /*
-                    $target_dir = "../uploads/";
-                    $target_file = $target_dir . basename($_FILES["myImage"]["name"]);
-                    $uploadOk = 1;
-                    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-                    // Check if image file is a actual image or fake image
-
-                        $check = getimagesize($_FILES["myImage"]["tmp_name"]);
-                        if($check !== false) {
-                        // echo "File is an image - " . $check["mime"] . ".";
-                            $uploadOk = 1;
-                        } else {
-                            echo "File is not an image.";
-                            $uploadOk = 0;
-                        }
-                        
-                        if (move_uploaded_file($_FILES["myImage"]["tmp_name"], $target_file)) {
-                    //        echo "The file ". basename( $_FILES["myImage"]["name"]). " has been uploaded.";
-                            $uploadOk = 1;
-                        } else {
-                            $uploadOk = 0;
-                            echo "Sorry, there was an error uploading your file.";
-                        }
-                    */
+                } else
+                {
                     
-                    $sql="INSERT INTO category (name) VALUES ('{$_POST['new_cat']}')";
+                    $target_dir = "../uploads/";
+$target_file = $target_dir . basename($_FILES["myImage"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+
+    $check = getimagesize($_FILES["myImage"]["tmp_name"]);
+    if($check !== false) {
+       // echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+    
+    if (move_uploaded_file($_FILES["myImage"]["tmp_name"], $target_file)) {
+//        echo "The file ". basename( $_FILES["myImage"]["name"]). " has been uploaded.";
+        $uploadOk = 1;
+    } else {
+        $uploadOk = 0;
+        echo "Sorry, there was an error uploading your file.";
+    }
+
+                    
+                    $sql="INSERT INTO item_category (Name,Path) VALUES ('{$_POST['category']}','{$target_file}')";
                     //echo $sql;
                     
                     $result = mysqli_query($link,$sql);
@@ -162,7 +163,7 @@ include_once('head.php');
         
     }
   
-       $sql="SELECT * FROM category";
+       $sql="SELECT * FROM item_category";
        $result = mysqli_query($link,$sql);
        if(mysqli_error($link))
        {
@@ -173,9 +174,9 @@ include_once('head.php');
        
        while($row=mysqli_fetch_assoc($result))
        {
-           echo "<tr> <td>{$row['id']}</td> <td>{$row['name']}</td> 
-                <td><button name='add_category.php?cat_id={$row['id']}&action=edit' onclick='edit(this);'>Edit</button></td>
-                <td><button name='add_category.php?cat_id={$row['id']}&action=delete' onclick='deletes(this);'>Delete</button></td>
+           echo "<tr> <td>{$row['Cat_id']}</td> <td>{$row['Name']}</td> 
+                <td><button name='add_category.php?cat_id={$row['Cat_id']}&action=edit' onclick='edit(this);'>Edit</button></td>
+                <td><button name='add_category.php?cat_id={$row['Cat_id']}&action=delete' onclick='deletes(this);'>Delete</button></td>
                 </tr>";
        }
        
@@ -187,13 +188,12 @@ include_once('head.php');
         echo "
         <form action='add_category.php' method='post' enctype='multipart/form-data'>
         <table>
-        <tr><td>Category Name</td><td><input type='text' name='new_cat'></td></tr>
+        <tr><td>Category Name</td><td><input type='text' name='category'></td></tr>
+        <tr><td>Upload image</td><td><input type='file' name='myImage' accept='image/*' /></td></tr>
         <tr><td colspan='2'><input type='submit' name='submit' value='submit' ></td></tr>
-        
         </table> ";
-        //<tr><td colspan='2'><button name='add_category.php?&action=add' onclick='add(this);'>Add</button></td></tr>
    ?>
-        
+  
   </div>
 <?php
     include_once('foot.php');
