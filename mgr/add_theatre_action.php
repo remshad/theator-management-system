a188cc68405b1ccaff01dc840719e84fb908d308
rahman <57436911+rahmancpy555@users.noreply.github.com>
2019-11-22 18:52:04 +0530

    abc

--- mgr/add_theatre_action.php
+++ mgr/add_theatre_action.php
@@ -5,6 +5,7 @@ include_once('dbs.php');
     {
       $theatre=$_POST['theatre'];
       $district=$_POST['district'];
+      $location=$_POST['location'];
       $mgr_id=$_COOKIE["t_id"];
       $count=mysqli_query($link,"select * from theatre where u_id='$mgr_id'");
       if(mysqli_num_rows($count)>0)
@@ -18,7 +19,8 @@ window.location.href='index.php';
     else
     {
       $_SESSION['screens']=$_POST['screens'];
-      $result= mysqli_query($link,"INSERT INTO `theatre`(`t_theatrename`,`district_id`,`u_id`) VALUES ('$theatre','$district','$mgr_id')");
+      //echo "INSERT INTO `theatre`(`t_theatrename`,`district_id`,`t_theatre_place`,`u_id`) VALUES ('$theatre','$district','$location','$mgr_id')";
+      $result= mysqli_query($link,"INSERT INTO `theatre`(`t_theatrename`,`district_id`,`t_theatre_place`,`u_id`) VALUES ('$theatre','$district','$location','$mgr_id')");
       if($result)
       {
           header("Location:add_screen.php");
